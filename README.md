c9db05a9784e70df1b9eb161758caecc110d98cf
Michael Stokoe <mstokoe0990@gmail.com>
2019-07-07 16:27:55 +0100

    Updated readme, added parameter for length

--- README.md
+++ README.md
@@ -34,15 +34,18 @@ class User extends Authenticatable
 ...
 
 ```
-Now that model has a relationship called `apiKey`, it is a one-to-one polymorphic relationship for the time-being, though one-to-many is planned for future a release.
+Now that model has a relationship called `apiKey`.
 
 ### Generate new API Token for a given record.
 Generate an API token for given record.
 ```php
 $user = App\User::first();
 $user->generateApiToken();
-
 // return ex: d96d7fb36e394c22bab8d4089f619752a3fe172effbdaad738d4276d81df72305373e207a7a91f8e18fc32cf1f9b6c6977d540f9a125c0746101d539
+
+// You can also set token length at time of generation by passing an integer to the generate method.
+$user->generateApiToken(12);
+// return ex: 4bfc264da5337994341981a2
 ```
 ## Get the API Token related to the record
 The property is appended to the record, so be careful when querying.
--- src/Tokenable.php
+++ src/Tokenable.php
@@ -13,16 +13,21 @@ trait Tokenable
 
     public function getApiTokenAttribute()
     {
-        return $this->attributes['api_token'] = $this->apiToken()->first()->token ?? null;
+        return $this->attributes['api_token'] = $this->apiToken()->orderBy('created_at', 'desc')->first()->token ?? null;
     }
 
-    public function generateApiToken()
+    public function generateApiToken(int $length = null)
     {
-        if (function_exists('random_bytes')) {
-            $token = bin2hex(random_bytes(config('tokenable.token_length')));
-        } else {
-            $token = bin2hex(openssl_random_pseudo_bytes(config('tokenable.token_length')));
+        if (!isset($length))
+        {
+            $length = config('tokenable.token_length');
         }
+
+        // if (function_exists('random_bytes')) {
+            // $token = bin2hex(random_bytes($length));
+        // } else {
+            $token = bin2hex(openssl_random_pseudo_bytes($length));
+        // }
         
         $apiToken = new ApiToken([
             'token' => $token,
